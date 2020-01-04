82dee2222cdf5fd06080b24e57de10a7cebb3565
Nsuliman <nsuliman88@gmail.com>
2020-01-04 21:56:15 +0200

    Second route Done , Data route

--- server.js
+++ server.js
@@ -15,11 +15,19 @@ server.use(cors());
 
 // Main Routes
 
+// Home Page Route 
 server.get('/', (req,res) => 
 {
     res.status(200).send('Home Page');
 });
 
+// Data Route 
+const object = { "Name" :'Nawal Suliman', "age" : 31 , "Specialization" : " Computer Engineering "};
+
+server.get('/data', (req,res)=>
+{
+    res.status(200).json(object);
+});
 
 
 server.listen(PORT , ()=> console.log(`Listens on port ${PORT}`));
